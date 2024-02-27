d1710e85185b25a5e08a444ebaef67605d6a20ed
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2024-02-27 17:20:17 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageMatting from './ImageMatting';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageMatting />
+    </div>
+  );
+}
+
+export default App;
